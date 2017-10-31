d2a56c4d21253621524fb204aa5bde590880506e
chatagiri <chatagiriii@gmail.com>
2017-10-31 21:10:53 +0900

    first commit

---
+++ src/PaperShaper/Controller.java
@@ -0,0 +1,77 @@
+package PaperShaper;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.awt.datatransfer.Clipboard;
+
+import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
+public class Controller{
+
+    // ControllerからStageへのアクセッサ
+    Stage thisStage;
+    void setThisStage(Stage stage) {
+        thisStage = stage;
+    }
+
+    @FXML
+    private TextField sourceField,resultField;
+
+    @FXML
+    private CheckBox overlayFlag,clearFlag;
+
+    // 実行ボタン降下時処理
+    @FXML
+    protected void doButtonAction(){
+        System.out.println(sourceField.getText());
+        String source = sourceField.getText();
+        // 改行文字を空白に置換
+        String result = source.replaceAll("\n", "");
+        resultField.setText(result);
+    }
+
+    // オーバーレイ是非処理
+    @FXML
+    private void doOverlayAction(){
+        if(overlayFlag.isSelected() == true)
+            thisStage.setAlwaysOnTop(true);
+        else
+            thisStage.setAlwaysOnTop(false);
+    }
+
+    // ソースクリック時フィールド自動クリア是非
+    @FXML
+    private void doSourceAction(){
+        if(clearFlag.isSelected() == true)
+            sourceField.clear();
+    }
+
+    @FXML
+    private void doClearAction(){
+        if(clearFlag.isSelected() == true)
+        sourceField.clear();
+    }
+
+    // クリップボードから貼り付け処理
+    @FXML
+    protected void doPasteAction() throws IOException, UnsupportedFlavorException {
+        Clipboard clip = Toolkit.getDefaultToolkit().getSystemClipboard();
+        // Clipboardからテキストを取得
+        String result = (String)clip.getData(DataFlavor.stringFlavor);
+        sourceField.setText(result);
+    }
+
+    // クリップボードにコピーする処理
+    @FXML
+    protected void doCopyAction(){
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        StringSelection selection = new StringSelection(resultField.getText());
+        clipboard.setContents(selection, selection);
+    }
+}
---
+++ src/PaperShaper/Main.java
@@ -0,0 +1,41 @@
+/**
+ * @author @chatagiriii
+ *
+ * JavaFXの練習を兼ねたクリップボードから張り付けた文字列の改行文字を空白文字に置換するモノです
+ * 連続作業に備えてウィンドウのオーバレイ表示，フィールドの自動クリアを実装しています
+ */
+
+package PaperShaper;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        // FXML読み込み
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Parent root = loader.load();
+
+        // Stage初期設定
+        stage.setTitle("コピペ整形くん");
+        stage.setScene(new Scene(root, 540, 160));
+        stage.setResizable(false);
+
+        // ControllerからStageにアクセス出来るように
+        Controller controller = (Controller)loader.getController();
+        controller.setThisStage(stage);
+
+        // Stage表示
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        // start内実行
+        launch(args);
+    }
+}
